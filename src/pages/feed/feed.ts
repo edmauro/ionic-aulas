e15beb2884a1ae4e774890304f340283b83dc9fc
edmauro <edmauro.morais@hotmail.com>
2018-11-17 17:12:46 -0300

    Primeiro commit aulas ionic

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+
+  nomeUser = "Edmauro Vindo do Código";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  somaDoisNumeros(num1: number, num2: number) {
+    alert(num1 + num2);
+  }
+
+  ionViewDidLoad() {
+    //this.somaDoisNumeros(10, 90);
+    // console.log('ionViewDidLoad FeedPage');
+  }
+}
